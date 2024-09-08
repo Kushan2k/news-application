7f231ede44d76c96a2f2cf030e606a88e8e73089
heshara2000 <hesharadananjanee@gmail.com>
2024-09-08 21:36:59 +0530

    create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../login.css'
 
 function LoginPage() {
   return (
-    <div>
-      login page
-      <Link to={'/register'}>Register</Link>
+    // <div>
+    //   login page
+    //   <Link to={'/register'}>Register</Link>
+    // </div>
+    <div className="login-page">
+      <div className="login-page-container">
+        {/* Left side for image */}
+        <div className="login-image">
+          <img src="../assets/login.png" alt="Login" />
+        </div>
+
+        {/* Right side for form */}
+        <div className="login-form">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+
+            <button type="submit" className="login-btn">Login</button>
+          </form>
+        </div>
+      </div>
     </div>
 
   )
