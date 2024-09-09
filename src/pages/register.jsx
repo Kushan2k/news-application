38e15863ff9580141a7e8c5b5f9c3bfa5c64401e
Heshara Dananjanee <93027216+heshara2000@users.noreply.github.com>
2024-09-09 22:49:16 +0530

    Bug fixed in the register page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom'
-import { z } from 'zod'
+import { z } from 'zod' 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth, firestore } from '../firebase.config';
