ffa4ba30be18c425929e72d3583b8c9c7fd435b4
Heshara Dananjanee <93027216+heshara2000@users.noreply.github.com>
2024-09-09 22:32:22 +0530

    Design the Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { toast } from 'react-toastify';
-import "../contact.css";
+import "../contact.css";   
 
 const Contact = () => {
   const form = useForm();
