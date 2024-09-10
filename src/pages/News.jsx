0c7b5f55cd821496bcf51bb77f9f4e032ac1368c
Kushan2k <kushangayantha001@gmail.com>
2024-09-10 09:55:35 +0530

    category news page edited/bug fix

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios'
-import { Loader2 } from 'lucide-react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { ArrowLeft, Heart, Loader2 } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useNavigation, useParams } from 'react-router-dom'
+import { Link, useNavigate, useNavigation, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { auth, firestore } from '../firebase.config'
+import { arrayRemove, arrayUnion, doc, getDoc, updateDoc } from 'firebase/firestore'
+import { createUniqueIdentifier } from '../utils'
 
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 function News() {
@@ -50,6 +54,12 @@ function News() {
   }, [])
   return (
     <div className="p-5">
+      <div className='flex items-center justify-start'>
+        <Link to={'/'} className='flex items-center justify-center gap-3'>
+          <ArrowLeft size={30} />
+          Back to Home
+        </Link>
+      </div>
       {
         loading && (
           <div className="flex h-20 mt-10 items-center justify-center">
@@ -60,11 +70,7 @@ function News() {
       {
         !loading && news?.map((article, index) => {
           return (
-            <div key={index} className="bg-white p-5 rounded-md shadow-md">
-              <h1 className="text-2xl font-bold">{article.title}</h1>
-              <p className="text-gray-500">{article.description}</p>
-              <a href={article.url} target="_blank" rel="noreferrer" className="text-blue-500">Read More</a>
-            </div>
+            <HorNewsCard key={index} article={article} />
           )
         })
       }
@@ -73,3 +79,142 @@ function News() {
 }
 
 export default News
+
+
+function HorNewsCard({ article }) {
+
+  const [isFav, setIsFav] = useState(false)
+  const [user, setUser] = useState(null)
+  const [loading, SetLoading] = useState(false)
+
+  async function add_to_fav() {
+
+    try {
+      SetLoading(true)
+      const ref = doc(firestore, 'favourites', user.uid)
+
+      const docSnap = await getDoc(ref)
+      const d = docSnap.data()
+
+
+      if (d?.favourites.length > 0) {
+
+        await updateDoc(ref, {
+          favourites: arrayUnion({
+            id: createUniqueIdentifier(article),
+            news: {
+              ...article
+            }
+          }
+          )
+        })
+      } else {
+        await updateDoc(ref, {
+          favourites: [{
+            id: createUniqueIdentifier(article),
+            news: {
+              ...article
+            }
+          }]
+        })
+      }
+      setIsFav(true)
+      toast.success("Added to favourites")
+    } catch (error) {
+      console.log(error)
+      toast.warn("Something went wrong")
+    } finally {
+      SetLoading(false)
+    }
+  }
+
+  async function remove_from_fav() {
+
+    try {
+      SetLoading(true)
+      const id = createUniqueIdentifier(article)
+      const ref = doc(firestore, 'favourites', user.uid)
+
+      const docSnap = await getDoc(ref)
+
+      await updateDoc(ref, {
+        favourites: arrayRemove({
+          id: id,
+          news: {
+            ...article
+          }
+        })
+      })
+
+      toast.success("Removed from favourites")
+      setIsFav(false)
+
+
+    } catch (error) {
+      toast.warn("Something went wrong")
+    } finally {
+      SetLoading(false)
+    }
+  }
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user)
+
+      }
+    })
+
+  }, [])
+
+  return (
+    <div className="bg-white p-5 rounded-md my-2  shadow-md relative">
+      <div className="absolute top-1 right-1">
+        {
+          user && (
+            isFav ? (
+              <button disabled={loading} onClick={() => {
+                remove_from_fav()
+              }} className='bg-transparent hover:scale-105 border-none p-3 absolute top-2 right-2 z-40'>
+                {
+                  loading ? (
+                    <svg className="animate-spin border-b-2 rounded-full border-green-600 h-5 w-5" viewBox="0 0 24 24">
+
+                    </svg>
+                  ) : (
+                    <Heart size={30} color='red' fill='red' className='hover:fill-white' />
+                  )
+                }
+
+              </button>
+            ) : (
+              <button disabled={loading} onClick={
+                () => {
+                  add_to_fav()
+                }
+              } className='bg-transparent hover:scale-105 border-none p-3 absolute top-2 right-2 z-40'>
+                {
+                  loading ? (
+                    <svg className="animate-spin border-b-2 rounded-full border-green-600 h-5 w-5" viewBox="0 0 24 24" >
+
+                    </svg>
+                  ) : (
+                    <Heart size={30} color='red' className='hover:fill-red-600' />
+                  )
+                }
+
+              </button>
+            )
+          )
+        }
+      </div>
+      <small className='text-gray-500'>{article.publishedAt.split('T')[0]}</small>
+      <br />
+      <small className='text-gray-600 capitalize font-semibold'>{article.author}</small>
+      <h1 className="text-xl font-bold">{article.title}</h1>
+      <p className="text-gray-500">{article.description}</p>
+      <a href={article.url} target="_blank" rel="noreferrer" className="text-blue-500">Read More</a>
+    </div>
+  )
+}
