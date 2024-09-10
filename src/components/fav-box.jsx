4cfb67a2046279d3734abc6d27eababa4e9576a7
Kushan2k <kushangayantha001@gmail.com>
2024-09-10 10:18:44 +0530

    remove from favourites added

--- src/components/fav-box.jsx
+++ src/components/fav-box.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { auth, firestore } from '../firebase.config';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { AlertCircleIcon } from 'lucide-react';
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { AlertCircleIcon, CircleCheck, X } from 'lucide-react';
+import { arrayRemove, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
+import { createUniqueIdentifier } from '../utils';
 
 function FavoriteBox() {
 
@@ -75,7 +76,7 @@ function FavoriteBox() {
               {
                 !loading && favourites.length > 0 && favourites.map((news, index) => (
 
-                  <ListItem key={index} text={news.news.title} />
+                  <ListItem user={user} key={index} art={news.news} />
 
                 ))
               }
@@ -96,35 +97,46 @@ function FavoriteBox() {
 export default FavoriteBox
 
 
-const ListItem = ({ text }) => {
+const ListItem = ({ art, user }) => {
+
+  async function remove_from_fav() {
+
+    try {
+
+      const id = createUniqueIdentifier(art)
+      const ref = doc(firestore, 'favourites', user.uid)
+
+      await updateDoc(ref, {
+        favourites: arrayRemove({
+          id: id,
+          news: {
+            ...art
+          }
+        })
+      })
+
+      toast.success("Removed from favourites")
+
+    } catch (error) {
+      toast.warn("Something went wrong")
+    }
+  }
+
   return (
     <li className="flex text-base text-body-color dark:text-dark-6">
-      <span className="mr-2.5 mt-0.5 text-secondary">
-        <svg
-          width={20}
-          height={20}
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g clipPath="url(#clip0_980_24852)">
-            <path
-              d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
-              fill="currentColor"
-            />
-            <path
-              d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
-              fill="currentColor"
-            />
-          </g>
-          <defs>
-            <clipPath id="clip0_980_24852">
-              <rect width={20} height={20} fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
+      <span className="mr-2.5 mt-0.5 text-secondary ">
+
+        <button onClick={() => {
+          remove_from_fav()
+        }} className='border-none bg-transparent  transition-colors hover:bg-red-500 '>
+          <X size={25} className='text-red-600 hover:text-white' />
+        </button>
+
+
       </span>
-      {text}
+
+
+      {art.title}
     </li>
   );
 };
