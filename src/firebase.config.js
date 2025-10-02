ab8fb90e35b19464bfd791bca65a24b0c5181256
Kushan Gayantha <kushangayantha001@gmail.com>
2025-10-02 09:29:19 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC1UEXJwro799_tml1rlub04I41KeFGtxA",
-  authDomain: "news-app-7d188.firebaseapp.com",
-  projectId: "news-app-7d188",
-  storageBucket: "news-app-7d188.appspot.com",
-  messagingSenderId: "96114946264",
-  appId: "1:96114946264:web:24503bee20556e2759e7ad",
-}
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const firestore = getFirestore(app)
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
